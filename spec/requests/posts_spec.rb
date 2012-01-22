b3cfa0447df4b43b912dd19011845251ce1cebd8
Lee Richmond <richmolj@gmail.com>
2012-01-22 15:36:12 -0500

    finishing up

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "Posts" do
+  def create_post(opts)
+    feature_setting = opts[:feature] == :off ? 'false' : 'true'
+    Factory(:site_configuration_option, :setting => 'destroy_posts', :value => feature_setting)
+    @post = Factory(:post, :destroyable => true)
+  end
+
+  # BASIC RSPEC
   describe "DELETE to destroy" do
     context "when feature is turned off" do
       before do
-        Factory(:site_configuration_option, :setting => 'destroy_posts', :value => false)
-        @post = Factory(:post)
+        create_post(:feature => :off)
       end
 
       it "should not delete a post" do
@@ -17,14 +23,38 @@ describe "Posts" do
 
     context "when feature is turned on" do
       before do
-        Factory(:site_configuration_option, :setting => 'destroy_posts', :value => true)
-        @post = Factory(:post, :destroyable => true)
+        create_post(:feature => :on)
       end
 
       it "should delete the post" do
         lambda {
           delete post_path(@post.to_param)
         }.should change { Post.count }.by(-1)
+      end
+    end
+  end
+
+  # CAPYBARA
+  describe "DELETE to destroy with capybara", :type => :request do
+    context "when the feature is turned off" do
+      before do
+        create_post(:feature => :off)
+      end
+
+      it "should not delete the post" do
+        visit(posts_path)
+        lambda { click_link('Destroy') }.should_not change { Post.count }
+      end
+    end
+
+    context "when the feature is turned on" do
+      before do
+        create_post(:feature => :on)
+      end
+
+      it "should not delete the post" do
+        visit(posts_path)
+        lambda { click_link('Destroy') }.should change { Post.count }.by(-1)
       end
     end
   end
